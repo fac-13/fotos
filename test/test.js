c7e3ed39c206fb4a320811edb2af34e3a945cc9b
isnotafunction <ivana.cosgrove@gmail.com>
2018-04-18 15:05:16 +0100

    adds failing test for home route relates #4'

--- test/test.js
+++ test/test.js
@@ -1,4 +1,15 @@
-const tape = require('tape'); 
-const request = require('supertest'); 
-const app = require ('../src/app.js');  
+const tape = require('tape');
+const request = require('supertest');
+const app = require('../src/app.js');
 
+tape('home route returns a status code of 200', t => {
+  request(app)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.statusCode, 200, 'Should return 200');
+      t.end();
+    });
+});
