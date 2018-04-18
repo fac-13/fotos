4ce66f42bbfc1a3b7aabd47f4b7e9e7fddb26a4f
isnotafunction <ivana.cosgrove@gmail.com>
2018-04-18 22:29:26 +0100

    displays single photo on click

--- src/model/database/queries.js
+++ src/model/database/queries.js
@@ -6,6 +6,12 @@ const getAll = () => {
   ON photos.user_id = users.id;`);
 };
 
+const getPhoto = imageID => {
+  return db.query(`SELECT users.username, photos.title, photos.date, photos.description, photos.image_url FROM photos
+  INNER JOIN users
+  ON photos.user_id = users.id WHERE photos.id = ${imageID};`);
+};
 module.exports = {
-  getAll
+  getAll,
+  getPhoto
 };
