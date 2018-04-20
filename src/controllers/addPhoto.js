dd45e594a884dbb56c5a24307bdeebac7a5a083b
Helen Zhou <helen.zhou6@gmail.com>
2018-04-20 10:12:37 +0100

    Bug fix with add photos

--- src/controllers/addPhoto.js
+++ src/controllers/addPhoto.js
@@ -6,8 +6,10 @@ exports.post = (req, res) => {
   const description = req.body.description;
   const imageUrl = req.body.imageURL;
   const username = req.session.username;
+  console.log(username);
   queries
-    .postPhoto(username, title, description, imageUrl)
+    .getUserId(username)
+    .then(result => queries.postPhoto(result[0].id, title, description, imageUrl))
     .then(res.redirect('/'))
     .catch(err => {
       console.log(`The error is: ${err.message}`);
